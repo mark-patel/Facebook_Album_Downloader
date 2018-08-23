11b2fdaca0f699be0602ac18548d899cbb68c136
markpatel0880 <42213320+markpatel0880@users.noreply.github.com>
2018-08-23 06:06:35 +0530

    Update fb.js

--- js/fb.js
+++ js/fb.js
@@ -24,7 +24,7 @@ function checkLoginState() {
 
 window.fbAsyncInit = function() {
 	FB.init({
-	appId      : '253545741848173',
+	appId      : 'Your Facebook App Id',
 	cookie     : true,  // enable cookies to allow the server to access 
 	                    // the session
 	xfbml      : true,  // parse social plugins on this page
